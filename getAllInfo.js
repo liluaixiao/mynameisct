c5876804067cf2e2b1ce3997c60df915e846684a
bill.chentian <bill.chentian@outlook.com>
2017-08-24 09:59:31 +0800

    3th

--- getAllInfo.js
+++ getAllInfo.js
@@ -1,34 +1,31 @@
 var superagent = require('superagent');
 var fs=require('fs');
-var file="./data/house.json";
+var file="./data/housepro.json";
 var result=JSON.parse(fs.readFileSync(file));
 
 
 
-var sk = 'VF9LNVjmCBi1gZU5rdkUTtGfDMS6GWLg';
+var sk = '你的百度地图sk';
 
 var addresses = result.simple;
 var i = -1;
 function change(){
 	i++;
 	superagent.get('http://api.map.baidu.com/geocoder/v2/')
-	  .query({address: addresses[i].address})
-	  .query({city: "贵阳市"})
+	  .query({location: addresses[i].location.lat+","+addresses[i].location.lng})
 	  .query({output: 'json'})
+	  .query({pois: 1})
 	  .query({ak: sk})
 	  .end(function(err, sres) {
 	  	var data = JSON.parse(sres.text);
 	  	 if (data.status==0) {
-	       addresses[i].location = data.result.location;
-	     }else{
-		   addresses[i].location = "";
-		   console.log('none'+"名字为"+addresses[i].title);
-		 }
-		 console.log("这是第"+i+"次转化,转化地址为"+addresses[i].location);
+	       addresses[i].addressComponent = data.result.addressComponent;
+	     }
+		 console.log("这是第"+i+"次转化");
 		 if(i==addresses.length-1){
 		 		var x = JSON.stringify(result);
 				console.log("都转化完了");
-				fs.appendFile('./data/newhouse.json', x, 'utf-8', function (err) {
+				fs.appendFile('./data/newhousepro.json', x, 'utf-8', function (err) {
 		            if (err) {
 		                console.log(err);
 		            }
